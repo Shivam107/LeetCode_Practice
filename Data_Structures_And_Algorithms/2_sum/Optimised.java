85f1ce2941ae7565f85fc8e893ac5a1537ac28bf
Shivam Suryawanshi <shivamsuryawanshi000@gmail.com>
2024-08-26 16:43:09 +0530

    optimised

---
+++ Data_Structures_And_Algorithms/2_sum/Optimised.java
@@ -0,0 +1,13 @@
+class Optimised {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer , Integer> complements = new HashMap<>();
+            for (int i = 0 ; i < nums.length ; i++){
+                Integer complementIndex = complements.get(nums[i]);
+                    if (complementIndex != null){
+                        return new int[]{i , complementIndex};
+                    }
+                    complements.put(target - nums[i] , i);
+            }
+       return nums ;
+    }
+}
